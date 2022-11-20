78febbbba4a27e11baceef24dd6b9059e87201b9
Hai Nguyen <hnguyen@northern.co>
2022-11-20 10:01:08 -0500

    hard code some links for the upper menu

--- modules/custom/hnp_upper_menu/src/Plugin/Block/UpperMenuBlock.php
+++ modules/custom/hnp_upper_menu/src/Plugin/Block/UpperMenuBlock.php
@@ -4,6 +4,7 @@ namespace Drupal\hnp_upper_menu\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Url;
+use Drupal\hnp_constants\Constants;
 
 /**
  * Provides 'Upper Menu' block.
@@ -20,14 +21,61 @@ class UpperMenuBlock extends BlockBase {
    * {@inheritDoc} 
    */
   public function build() {
+    /** @var array $build */
     $build = [];
 
-    $build['drupal'] = [
+    // WEB DEV
+
+    $build['web']['drupal_7'] = [
       '#type' => 'link',
-      '#url' => Url::fromRoute('<front>'),
-      '#title' => $this->t('Drupal'),
+      '#url' => Url::fromRoute(Constants::DEFAULT_ROUTE),
+      '#title' => $this->t('Drupal 7'),
       '#attributes' => [
-        'id' => '',
+        'id' => 'web--drupal-7',
+        'class' => [],
+      ],
+    ];
+
+    $build['web']['drupal_9'] = [
+      '#type' => 'link',
+      '#url' => Url::fromRoute(Constants::DEFAULT_ROUTE),
+      '#title' => $this->t('Drupal 9+'),
+      '#attributes' => [
+        'id' => 'web--drupal-9',
+        'class' => [],
+      ],
+    ];
+
+    // MOBILE DEV
+
+    $build['mobile']['ios'] = [
+      '#type' => 'link',
+      '#url' => Url::fromRoute(Constants::DEFAULT_ROUTE),
+      '#title' => $this->t('iOS'),
+      '#attributes' => [
+        'id' => 'mobile--ios',
+        'class' => [],
+      ],
+    ];
+
+    // GENERAL
+
+    $build['general']['resume'] = [
+      '#type' => 'link',
+      '#url' => Url::fromRoute(Constants::DEFAULT_ROUTE),
+      '#title' => $this->t('My Resume'),
+      '#attributes' => [
+        'id' => 'general--resume',
+        'class' => [],
+      ],
+    ];
+
+    $build['general']['contact'] = [
+      '#type' => 'link',
+      '#url' => Url::fromRoute(Constants::DEFAULT_ROUTE),
+      '#title' => $this->t('Say Hi'),
+      '#attributes' => [
+        'id' => 'general--contact',
         'class' => [],
       ],
     ];
@@ -36,3 +84,7 @@ class UpperMenuBlock extends BlockBase {
   }
 
 }
+
+/*
+@TODO: replace the hard-code route Constants::DEFAULT_ROUTE.
+*/
