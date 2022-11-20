fc8fde60ac75dff2db15f5d6b6490711ecd6f38b
Hai Nguyen <hnguyen@northern.co>
2022-11-20 12:53:34 -0500

    quick save

--- themes/custom/digitalechidna/de_theme/gulpfile.js
+++ themes/custom/digitalechidna/de_theme/gulpfile.js
@@ -1,412 +1,197 @@
-
-
-
-// dont' forget config stuff in settings.local.php
+ // dont' forget config stuff in settings.local.php
 
 'use strict';
-var beep = require('beepbeep')
 var git = require('gulp-git');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var sassGlob = require('gulp-sass-glob');
 var uglify = require('gulp-uglify');
-var babel = require('gulp-babel');
-var pipeline = require('readable-stream').pipeline;
-var browserSync = require('browser-sync').create();
-var runSequence = require('gulp4-run-sequence');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
 var rename = require('gulp-rename');
 var plumber = require ('gulp-plumber');
+var gutil = require('gulp-util');
 var del = require('del');
 var autoprefixer = require('gulp-autoprefixer');
+var urlAdjuster = require('gulp-css-url-adjuster');
 var imagemin = require('gulp-imagemin');
 var sassLint = require('gulp-sass-lint');
-var webpackStream = require('webpack-stream');
-var webpackConfig = require('./webpack.config.js');
-var webpackProdConfig = require('./webpack.config.prod.js');
-
-
-
-/* ----------------------------- */
-/*         Configuration         */
-/* ----------------------------- */
-
-
 
 // Config defaults.
 var config = {
-  proxyTarget: undefined
+  proxy_url: undefined
 };
 
-/*
- * Configuration Overrides
- * Include local file for overriding config, don't fail if it doesn't exist.
- */
+// Include local file for overriding config, don't fail if it doesn't exist.
 try {
-  config = require('./config.json');
+  config = require('./config/proxy_url.json');
 } catch (error) {
-
 }
 
-config.drupal = {
-  templatesDir: 'templates'
-};
-
-// Helper functions.
-function isDirectory(dir) {
-  try {
-    return fs.statSync(dir).isDirectory();
-  }
-  catch (err) {
-    return false;
-  }
-}
-
-/*
- * Error Reporting
- * Report errors but keep on gulpin'.
- */
+// report errors but continue gulpin'
 var onError = function (err) {
-  beep();
+  gutil.beep();
   console.log(err.messageFormatted);
   this.emit('end'); // super crit
 };
 
+//////////////////////////////////////
+// Begin Gulp Tasks
+//////////////////////////////////////
 
+////// DEVELOPMENT
+//////////////////
 
-/* ----------------------------- */
-/* Begin Development Gulp Tasks  */
-/* ----------------------------- */
-
-
-
-/*
- * SCSS compile task
- * Compiles scss files set in src() to dest()
- */
-
+// SASS compile task
 var sassBuild = function() {
   return gulp
-    .src('src/sass/**/*.scss')
+    .src('./src/sass/**/*.scss')
     .pipe(plumber({
       errorHandler: onError
     }))
     .pipe(sourcemaps.init({largeFile: true}))
-    .pipe(sassGlob())
     .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write('maps'))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist/css'));
 };
 
-gulp.task('sass', gulp.series(sassBuild));
+gulp.task('sass', sassBuild);
 
-
-/*
- * JS compile task
- * Compiles js in src() to dest()
- * Run JS through Babel.
- * Suffix .min.js to filename
- */
-
+// Rename and move Javascript task
 var jsBuild = function() {
-  return gulp.src('src/scripts/js/**/*.js', '!src/scripts/js/**/*.webpack.js')
-             .pipe(plumber({
-               errorHandler: onError
-             }))
-             .pipe(babel({
-               presets: ['@babel/preset-env'],
-               ignore: [
-                 "src/scripts/js/vendor",
-               ]
-             }))
-             .pipe(rename(function (path) {
-               path.basename += ".min";
-               path.extname = ".js";
-             }))
-             .pipe(gulp.dest('dist/scripts/js'));
+  return gulp.src('./src/scripts/js/**/*.js')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(rename(function (path) {
+      path.basename += ".min";
+      path.extname = ".js";
+    }))
+    .pipe(gulp.dest('./dist/scripts/js'));
 };
 
-var jsWebpackBuild = function() {
-  return gulp.src('src/scripts/js/**/*.webpack.js')
-             .pipe(webpackStream(webpackConfig, null, function(err, stats) {
-               /* Use stats to do more things if needed */
-             })).on('error',function (err) {
-      console.error('WEBPACK ERROR', err);
-      this.emit('end'); // Don't stop the rest of the task
-    })
-             .pipe(gulp.dest('dist/scripts/js'));
-};
+gulp.task('js', jsBuild);
 
-gulp.task('js', gulp.series(jsBuild));
-gulp.task('webpackJS', gulp.series(jsWebpackBuild));
-
-
-/*
-* Move image assets task
-* Moves images in src() to dest()
-*/
-
+// Move image assets task
 var imageBuild = function() {
-  return gulp.src('src/img/**/*.!(svg)')
-             .pipe(gulp.dest('dist/img'));
+  return gulp.src('./src/img/**/*')
+    .pipe(gulp.dest('./dist/img'));
 };
 
-gulp.task('images', gulp.series(imageBuild));
+gulp.task('images', imageBuild);
 
 
-/*
-* Move font assets task
-* Moves fonts in src() to dest()
-*/
-
+// Move font assets task
 var fontBuild = function() {
-  return gulp.src('src/fonts/**/*')
-             .pipe(gulp.dest('dist/fonts'));
+  return gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts'));
 };
 
-gulp.task('fonts', gulp.series(fontBuild));
+gulp.task('fonts', fontBuild);
 
-
-/**
- * Generates Pattern Lab front-end.
- */
-
-// if (patternlab) {
-//   gulp.task('pl:generate', function () {
-//       return run('php ' + config.patternLab.dir + '/core/console --generate').exec();
-//   });
-// }
-
-
-/**
- * Calls BrowserSync reload.
- */
-
-gulp.task('bs:reload', function () {
-  browserSync.reload();
-});
-
-// BrowserSync Reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
-
-
-
-
-/* ----------------------------- */
-/* Watch */
-/* ----------------------------- */
-
-
-
-/*
-* Watch Task
-* Watches for changes in the directories defined
-* runs tasks depending on directory changed
-*/
-
+// Watch Task (for new changes from the following)
 gulp.task('watch', function() {
-  browserSync.init({
-    proxy: {
-      target: config.proxyTarget
-    },
-    open: true,
-    notify: false
-  });
-  gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
-  gulp.watch(['src/scripts/js/**/*.js', '!src/scripts/js/**/*.webpack.js'], gulp.series('js'));
-  gulp.watch('src/scripts/js/**/*.webpack.js', gulp.series('webpackJS'));
-  gulp.watch('src/img/**/*', gulp.series('images'));
-  gulp.watch('src/fonts/**/*', gulp.series('fonts'));
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/scripts/js/**/*.js', ['js']);
+  gulp.watch('./src/img/**/*', ['images']);
+  gulp.watch('./src/fonts/**/*', ['fonts']);
 });
 
-
-
-
-/* ----------------------------- */
-/* Other */
-/* ----------------------------- */
-
-
+// BrowserSync Task (refresh browser if files change)
+gulp.task('browserSync', function() {
+  browserSync.init([
+    ('./dist/css/*.css'),
+    ('./dist/scripts/js/**/*.js'),
+    ('./dist/img/**/*'),
+    ('./dist/fonts/**/*')
+  ],{
+     proxy: "http://10.18.14.80/napoleon.localhost/docroot"
+  });
+});
 
 // Lint Sass
-
 gulp.task('lint-sass', function () {
   return gulp.src('./src/sass/**/*.scss')
-             .pipe(sassLint())
-             .pipe(sassLint.format())
-  // .pipe(sassLint.failOnError())
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    // .pipe(sassLint.failOnError())
 });
 
+// Git rebase, fixcss and fixjs have to be completed first.
+gulp.task('rebase-continue', ['fixcss', 'fixjs'], function() {
+  git.exec({
+    args: 'rebase --continue'
+  });
+});
 
 // Recompile Sass and git add the files.
-
 gulp.task('fix-css', function() {
   return sassBuild().pipe(git.add());
 });
-
 
 // Recompile JS and git add the files.
 // Wait until fixcss runs, we don't want two git add operations happening at the
 // same time.
 // @todo Combine streams.
-
-gulp.task('fix-js', gulp.series('fix-css', function() {
+gulp.task('fix-js', ['fix-css'], function() {
   return jsBuild().pipe(git.add());
-}));
+});
 
-// Git rebase, fixcss and fixjs have to be completed first.
-
-gulp.task('rebase-continue', gulp.series('fix-css', 'fix-js', function() {
-  git.exec({
-    args: 'rebase --continue'
-  });
-}));
-
-gulp.task('fix', gulp.series('fix-css', 'fix-js', 'rebase-continue'));
+gulp.task('fix', ['fix-css', 'fix-js', 'rebase-continue']);
 
 
 // Server Tasks
-
-gulp.task('default', function(done) {
-  runSequence('watch', done);
+gulp.task('default', function(callback) {
+  runSequence('watch','browserSync',callback);
 });
 
 
-
-/* ///////////////////////////// */
-/*             BUILDS            */
-/* ///////////////////////////// */
-
-
+////// PRODUCTION
+/////////////////
 
 // Delete all files in dist directory in prep for final build
-
-gulp.task('clean', function(done) {
-  return del([
-    './dist/**/*'
+gulp.task('clean', function() {
+  del([
+      './dist/**/*'
   ]).then(paths => {
     console.log('Deleted files and folders: \n', paths.join('\n'));
   });
 });
 
+//  Build Task for production
+gulp.task('build', function(){
 
-//  Build All Task for production
-
-gulp.task('build-raw', function(done){
-
-  // rebuild pixel based images and minify
-  gulp.src('src/img/**/*.!(svg)')
-      .pipe(gulp.dest('dist/img/'));
-
-  // rebuild pixel based images and minify
-  gulp.src('src/img/**/*.svg')
-      .pipe(gulp.dest('dist/img/'));
-
-  // rebuild fonts
-  gulp.src('src/fonts/**/*')
-      .pipe(gulp.dest('dist/fonts'));
+  // rebuild images and mimify
+  gulp.src('./src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img/'));
 
   // rebuild css
-  gulp.src('src/sass/**/*.scss')
-      .pipe(sassGlob())
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('dist/css'));
-
-  gulp.src('src/scripts/js/**/*.webpack.js')
-      .pipe(webpackStream(webpackConfig, null, function(err, stats) {
-        /* Use stats to do more things if needed */
-      })).on('error',function (err) {
-        console.error('WEBPACK ERROR', err);
-        this.emit('end'); // Don't stop the rest of the task
-      })
-      .pipe(gulp.dest('dist/scripts/js'));
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(urlAdjuster({
+      replace: ['../../src/img/','../../dist/img/'],
+    }))
+    .pipe(gulp.dest('./dist/css'));
 
   // rebuild and uglify js
-  return pipeline(
-    gulp.src(['src/scripts/js/**/*.js', '!src/scripts/js/**/*.webpack.js']),
-    babel({
-      presets: ['@babel/preset-env'],
-      ignore: [
-        "src/scripts/js/vendor",
-      ]
-    }),
-    rename(function (path) {
+  gulp.src('./src/scripts/js/**/*.js')
+    .pipe(rename(function (path) {
       path.basename += ".min";
       path.extname = ".js"
-    }),
-    uglify(),
-    gulp.dest('dist/scripts/js')
-  );
-
-  done();
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/scripts/js'));
 });
 
 
-//  Build All Task for production
-
-gulp.task('build-all', function(done){
-
-  // rebuild pixel based images and minify
-  gulp.src('src/img/**/*.!(svg)')
-      .pipe(imagemin())
-      .pipe(gulp.dest('dist/img/'));
-
-  // rebuild pixel based images and minify
-  gulp.src('src/img/**/*.svg')
-      .pipe(gulp.dest('dist/img/'));
-
-  // rebuild fonts
-  gulp.src('src/fonts/**/*')
-      .pipe(gulp.dest('dist/fonts'));
-
-  // rebuild css
-  gulp.src('src/sass/**/*.scss')
-      .pipe(sassGlob())
-      .pipe(sass())
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('dist/css'));
-
-  gulp.src('src/scripts/js/**/*.webpack.js')
-      .pipe(webpackStream(webpackProdConfig, null, function(err, stats) {
-        /* Use stats to do more things if needed */
-      })).on('error',function (err) {
-        console.error('WEBPACK ERROR', err);
-        this.emit('end'); // Don't stop the rest of the task
-      })
-      .pipe(gulp.dest('dist/scripts/js'));
-
-  // rebuild and uglify js
-  return pipeline(
-    gulp.src(['src/scripts/js/**/*.js', '!src/scripts/js/**/*.webpack.js']),
-    babel({
-      presets: ['@babel/preset-env'],
-      ignore: [
-        "src/scripts/js/vendor",
-      ]
-    }),
-    rename(function (path) {
-      path.basename += ".min";
-      path.extname = ".js"
-    }),
-    uglify(),
-    gulp.dest('dist/scripts/js')
-  );
-
-  done();
-});
-
-
-// Clean out all assets and re-compile them for Development
-
-gulp.task('build-raw', gulp.series('clean', 'build-raw'));
-
-
-// Clean out all assets and re-compile them for Production
-
-gulp.task('build', gulp.series('clean', 'build-all'));
+// final task for production site
+// gulp.task('build-prod', function(callback) {
+//   runSequence('build-clean','build-all',callback);
+// });
