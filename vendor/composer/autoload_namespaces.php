5c92d9dcd33253ff0dbac52a38b60c8031c79419
Hai Nguyen <hnguyen@northern.co>
2023-01-03 11:52:46 -0500

    install phpunit/phpunit symfony/phpunit-bridge behat/behat drupal/drupal-extension phpspec/prophecy-phpunit; create core/phpunit.xml file (untrackable)

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,9 +6,18 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'WebDriver' => array($vendorDir . '/instaclick/php-webdriver/lib'),
     'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'Stack' => array($vendorDir . '/stack/builder/src'),
+    'Drupal\\Tests\\Driver' => array($vendorDir . '/drupal/drupal-driver/tests'),
+    'Drupal\\MinkExtension' => array($vendorDir . '/drupal/drupal-extension/src'),
+    'Drupal\\Exception' => array($vendorDir . '/drupal/drupal-extension/src'),
+    'Drupal\\DrupalExtension' => array($vendorDir . '/drupal/drupal-extension/src'),
+    'Drupal\\Drupal' => array($vendorDir . '/drupal/drupal-extension/src'),
+    'Drupal\\Driver' => array($vendorDir . '/drupal/drupal-driver/src'),
     'Console' => array($vendorDir . '/pear/console_getopt'),
+    'Behat\\MinkExtension' => array($vendorDir . '/friends-of-behat/mink-extension/src'),
+    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
     'Archive_Tar' => array($vendorDir . '/pear/archive_tar'),
     '' => array($vendorDir . '/pear/pear-core-minimal/src'),
 );
