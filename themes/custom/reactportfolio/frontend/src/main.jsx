86c2c1b8613348fdae7241bfa88d8ba9f468a7ec
Hai Nguyen <hnguyen@northern.co>
2023-03-30 08:03:11 -0400

    fake dynamic list of client projects

--- themes/custom/reactportfolio/frontend/src/main.jsx
+++ themes/custom/reactportfolio/frontend/src/main.jsx
@@ -9,6 +9,11 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    children: [
+      {
+        
+      }
+    ],
   }
 ]);
 
--- themes/custom/reactportfolio/frontend/src/routes/root.jsx
+++ themes/custom/reactportfolio/frontend/src/routes/root.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+
+const CLIENT_PROJECTS = [
+  {
+    id: '1',
+    title: 'KHSC',
+  },
+  {
+    id: '2',
+    title: 'Kaufman Hall',
+  },
+];
 
 export default function Root() {
   return (<div>
     <ul>
-      <li>Kingston Health Service Centre</li>
-      <li>York</li>
+      {CLIENT_PROJECTS.map(each => <li>
+          <Link to={`client-projects/${each.id}`}>{each.title}</Link>
+        </li> 
+      )}
     </ul>
   </div>);
 }
