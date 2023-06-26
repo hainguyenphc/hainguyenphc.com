3d2d3488af46c13b77b57b8047f34e77429ed9c4
Hai Nguyen <hnguyen@northern.co>
2023-06-26 15:16:38 -0400

    composer install drupal/ckeditor_templates

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -38,15 +38,15 @@ class ComposerAutoloaderInit779fe0ccaafbd8cb02590a7b87080481
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit779fe0ccaafbd8cb02590a7b87080481::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
